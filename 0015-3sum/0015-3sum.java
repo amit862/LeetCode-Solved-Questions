5eb81570a54f8612b249739355b79c2c0ba783a9
Amit Verma <77237968+amit862@users.noreply.github.com>
2022-11-11 00:18:24 +0530

    Time: 28 ms (87.81%), Space: 59.7 MB (38.75%) - LeetHub

---
+++ 0015-3sum/0015-3sum.java
@@ -0,0 +1,40 @@
+class Solution {
+    List<List<Integer>> res = new ArrayList();
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0; i<nums.length; i++) {
+            //dublicate a
+            if(i == 0 || nums[i-1] != nums[i]){
+               twoSumSorted(i+1,nums.length-1,nums,0-nums[i]);
+            }
+        }
+        return res;
+    }
+    
+    void twoSumSorted(int i, int j, int nums[], int target) {
+        int a1 = nums[i-1];
+        while(i<j) { //search space
+            if(nums[i]+nums[j]>target) {
+                j--;
+            } else if(nums[i]+nums[j]<target) {
+                i++;
+            } else {
+                List<Integer> list = new ArrayList();
+                list.add(a1);
+                list.add(nums[i]);
+                list.add(nums[j]);
+                
+                res.add(list);
+                
+                //duplicate b
+                while(i<j && nums[i]==nums[i+1])i++;
+                //duplicate c
+                while(i<j && nums[j]==nums[j-1])j--;
+                
+                
+                i++;
+                j--;
+            }
+        }
+    }
+}
