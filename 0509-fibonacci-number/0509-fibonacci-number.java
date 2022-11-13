43ea925cafe1cf59d6290c7538cd5092c57ba673
Amit Verma <77237968+amit862@users.noreply.github.com>
2022-11-13 18:33:24 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (61.80%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int fib(int n) {
+        
+        int a=0, b=1, count=2, c = 0;
+        if(n==0 || n==1) return n;
+        
+        while(count <= n) {
+            c = a + b;
+            a = b;
+            b = c;
+            count++;
+        }
+        return c;
+    }
+}
